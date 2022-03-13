59ae4d3ab32ec02dbf5a5830d47c6bcdb057c9a7
katakumby <admin@executeit.pl>
2022-03-13 15:32:10 +0100

    Finally working swap on orca

--- src/components/routes/PendingRoute.tsx
+++ src/components/routes/PendingRoute.tsx
@@ -10,23 +10,71 @@ import { Amount } from '../sections/Amount';
 import { PoweredBy } from '../sections/PoweredBy';
 import { QRCode } from '../sections/QRCode';
 import * as css from './PendingRoute.module.pcss';
-import BigNumber from "bignumber.js";
-import { loadPoolInfo, PoolTransactions } from '@project-serum/pool';
+import { getOrca, OrcaFarmConfig, OrcaPoolConfig, Network } from "@orca-so/sdk";
+import Decimal from "decimal.js";
 export const PendingRoute: FC = () => {
     const { symbol, connectWallet,splToken,recipient } = useConfig();
     const { amount, reset } = usePayment();
-    const { publicKey,sendTransaction } = useWallet();
+    const { publicKey,sendTransaction,signTransaction,wallet} = useWallet();
     const { setVisible } = useWalletModal();
 
     const { connection } = useConnection();
 
+    const swap = async function(){
+        const orca = getOrca(connection, Network.DEVNET);
+
+        const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
+        const solToken = orcaSolPool.getTokenB();
+        const solAmount = new Decimal(0.1);
+        const quote = await orcaSolPool.getQuote(solToken, solAmount);
+        const orcaAmount = quote.getMinOutputAmount();
+
+        console.log(`Swap ${solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA`);
+        const swapPayload = await orcaSolPool.swap(new PublicKey(publicKey.toString()), solToken, solAmount, orcaAmount);
+        // const swapTxId = await swapPayload.execute();
+
+        const opts = { signers: swapPayload.signers };
+
+        console.log("Transaction preview",swapPayload.transaction)
+        //let xy = await sendTransaction(swapPayload.transaction,connection, opts);
+        const signature = await sendTransaction(swapPayload.transaction, connection, opts);
+        console.log("signature:", signature);
+        let xy  = await connection.confirmTransaction(signature, 'processed');
+        console.log("signature:", xy);
+        return;
+    }
     const onClick = async  function(){
+        if(!publicKey)return;
+        const orca = getOrca(connection, Network.DEVNET);
 
-        let poolInfo = await loadPoolInfo(connection, new PublicKey('8YnCv62yA1WXuReeYqBFjT7DZBH2A8uJt5HfHPfR7wtK'));
-        console.log(poolInfo.state);
+        const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
+        const solToken = orcaSolPool.getTokenB();
+        const solAmount = new Decimal(0.1);
+        const quote = await orcaSolPool.getQuote(solToken, solAmount);
+        const orcaAmount = quote.getMinOutputAmount();
+        // const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
+        const { maxTokenAIn, maxTokenBIn, minPoolTokenAmountOut } = await orcaSolPool.getDepositQuote(
+            orcaAmount,
+            solAmount
+        );
 
-        if(1==1)return;
+        console.log(
+            `Deposit at most ${maxTokenBIn.toNumber()} SOL and ${maxTokenAIn.toNumber()} ORCA, for at least ${minPoolTokenAmountOut.toNumber()} LP tokens`
+        );
+        const poolDepositPayload = await orcaSolPool.deposit(
+            new PublicKey(publicKey.toString()),
+            maxTokenAIn,
+            maxTokenBIn,
+            minPoolTokenAmountOut
+        );
 
+        let xy = await sendTransaction(poolDepositPayload.transaction,connection);
+        console.log("Swapped:", xy);
+
+        // const poolDepositTxId = await poolDepositPayload.execute();
+        // console.log("Pool deposited:", poolDepositTxId, "\n");
+
+return;
         if (!publicKey || !amount){
             console.log('WalletNotConnectedError');
             console.log(amount);
@@ -63,7 +111,8 @@ export const PendingRoute: FC = () => {
                 </div>
                 <div className={css.scan}>Scan this code with your Solana Pay wallet</div>
                 <div className={css.confirm}>You'll be asked to approve the transaction</div>
-                <button onClick={payNow}>requestPayment</button>
+                <button onClick={swap}>requestPayment1</button>
+                <button onClick={payNow}>requestPayment2</button>
             </div>
             <div className={css.footer}>
                 <PoweredBy />
