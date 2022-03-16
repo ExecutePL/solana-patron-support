ce76d985a5bc995056c364c0e45755443ea64110
Bruno Cisowski <60900550+brunocisowski@users.noreply.github.com>
2022-03-16 13:20:29 +0100

    Revert "Collateral pay"

--- src/components/routes/PendingRoute.tsx
+++ src/components/routes/PendingRoute.tsx
@@ -10,119 +10,18 @@ import { Amount } from '../sections/Amount';
 import { PoweredBy } from '../sections/PoweredBy';
 import { QRCode } from '../sections/QRCode';
 import * as css from './PendingRoute.module.pcss';
-import { getOrca, OrcaFarmConfig, OrcaPoolConfig, Network } from "@orca-so/sdk";
-import Decimal from "decimal.js";
+import BigNumber from 'bignumber.js';
+
 export const PendingRoute: FC = () => {
     const { symbol, connectWallet, splToken, recipient } = useConfig();
     const { amount, reset } = usePayment();
-    const { publicKey,sendTransaction,signTransaction,wallet} = useWallet();
+    const { publicKey, sendTransaction } = useWallet();
     const { setVisible } = useWalletModal();
 
     const { connection } = useConnection();
-    const orca = getOrca(connection, Network.DEVNET);
-    const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
-    const swapSolForOrca = async function(){
-        if(!publicKey) return;
 
-        const solToken = orcaSolPool.getTokenB();
-        const solAmount = new Decimal(0.1);
-        const quote = await orcaSolPool.getQuote(solToken, solAmount);
-        const orcaAmount = quote.getMinOutputAmount();
-
-        console.log(`Swap ${solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA`);
-        const swapPayload = await orcaSolPool.swap(publicKey, solToken, solAmount, orcaAmount);
-        // const swapTxId = await swapPayload.execute();
-
-        const opts = { signers: swapPayload.signers };
-
-        console.log("Transaction preview",swapPayload.transaction)
-        //let xy = await sendTransaction(swapPayload.transaction,connection, opts);
-        const signature = await sendTransaction(swapPayload.transaction, connection, opts);
-        console.log("signature:", signature);
-        let xy  = await connection.confirmTransaction(signature, 'processed');
-        console.log("signature:", xy);
-        return;
-    }
-    const depositSwappedForLP = async function(){
-        if(!publicKey) return;
-
-        const solToken = orcaSolPool.getTokenB();
-        const solAmount = new Decimal(0.1);
-        const quote = await orcaSolPool.getQuote(solToken, solAmount);
-        const orcaAmount = quote.getMinOutputAmount();
-
-        /*** Pool Deposit ***/
-            // 4. Deposit SOL and ORCA for LP token
-        const { maxTokenAIn, maxTokenBIn, minPoolTokenAmountOut } = await orcaSolPool.getDepositQuote(
-                orcaAmount,
-                solAmount
-            );
-
-        console.log(
-            `Deposit at most ${maxTokenBIn.toNumber()} SOL and ${maxTokenAIn.toNumber()} ORCA, for at least ${minPoolTokenAmountOut.toNumber()} LP tokens`
-        );
-        const poolDepositPayload = await orcaSolPool.deposit(
-            publicKey,
-            maxTokenAIn,
-            maxTokenBIn,
-            minPoolTokenAmountOut
-        );
-        // const poolDepositTxId = await poolDepositPayload.execute();
-
-        const opts = { signers: poolDepositPayload.signers };
-        const signature = await sendTransaction(poolDepositPayload.transaction, connection, opts);
-        console.log("signature:", signature);
-        let xy  = await connection.confirmTransaction(signature, 'processed');
-        console.log("signature:", xy);
-    }
-
-    const farmDeposit= async function(){
-        if(!publicKey) return;
-        const lpBalance = await orcaSolPool.getLPBalance(publicKey);
-        const orcaSolFarm = orca.getFarm(OrcaFarmConfig.ORCA_SOL_AQ);
-        const farmDepositPayload = await orcaSolFarm.deposit(publicKey, lpBalance);
-        // const farmDepositTxId = await farmDepositPayload.execute();
-        // console.log("Farm deposited:", farmDepositTxId, "\n");
-
-        const opts = { signers: farmDepositPayload.signers };
-        const signature = await sendTransaction(farmDepositPayload.transaction, connection, opts);
-        console.log("signature:", signature);
-        let xy  = await connection.confirmTransaction(signature, 'processed');
-        console.log("signature:", xy);
-    }
-    const onClick = async  function(){
-        if(!publicKey)return;
-        const orca = getOrca(connection, Network.DEVNET);
-
-        const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
-        const solToken = orcaSolPool.getTokenB();
-        const solAmount = new Decimal(0.1);
-        const quote = await orcaSolPool.getQuote(solToken, solAmount);
-        const orcaAmount = quote.getMinOutputAmount();
-        // const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
-        const { maxTokenAIn, maxTokenBIn, minPoolTokenAmountOut } = await orcaSolPool.getDepositQuote(
-            orcaAmount,
-            solAmount
-        );
-
-        console.log(
-            `Deposit at most ${maxTokenBIn.toNumber()} SOL and ${maxTokenAIn.toNumber()} ORCA, for at least ${minPoolTokenAmountOut.toNumber()} LP tokens`
-        );
-        const poolDepositPayload = await orcaSolPool.deposit(
-            new PublicKey(publicKey.toString()),
-            maxTokenAIn,
-            maxTokenBIn,
-            minPoolTokenAmountOut
-        );
-
-        let xy = await sendTransaction(poolDepositPayload.transaction,connection);
-        console.log("Swapped:", xy);
-
-        // const poolDepositTxId = await poolDepositPayload.execute();
-        // console.log("Pool deposited:", poolDepositTxId, "\n");
-
-return;
-        if (!publicKey || !amount){
+    const onClick = async function () {
+        if (!publicKey || !amount) {
             console.log('WalletNotConnectedError');
             console.log(amount);
             return;
@@ -131,14 +30,9 @@ return;
             splToken,
         });
         await sendTransaction(transaction1, connection);
-    }
-    const checkFarmRewards = async function(){
-        if(!publicKey)return;
-        const orcaSolFarm = orca.getFarm(OrcaFarmConfig.ORCA_SOL_AQ);
-        let harvestable = await orcaSolFarm.getHarvestableAmount(publicKey)
-        console.log("harvestable",harvestable.toNumber());
-    }
-    function payNow(){
+    };
+
+    function payNow() {
         onClick();
     }
     useEffect(() => {
@@ -164,11 +58,7 @@ return;
                 </div>
                 <div className={css.scan}>Scan this code with your Solana Pay wallet</div>
                 <div className={css.confirm}>You'll be asked to approve the transaction</div>
-                <button onClick={swapSolForOrca}>swap SOL for orca token</button>
-                <button onClick={depositSwappedForLP}>deposit sol and orca to pool for LP token</button>
-                <button onClick={farmDeposit}>deposit LP tokens to farm</button>
-                <button onClick={checkFarmRewards}>checkFarmRewards</button>
-                <button onClick={payNow}>requestPayment2</button>
+                <button onClick={payNow}>requestPayment</button>
             </div>
             <div className={css.footer}>
                 <PoweredBy />
