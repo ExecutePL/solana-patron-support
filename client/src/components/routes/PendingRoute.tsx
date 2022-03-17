eee95026930ca8f3a6cc49be9f6c8f746805a1c1
wojcikfil <94838246+wojcikfil@users.noreply.github.com>
2022-03-17 18:22:05 +0100

    fix: import

--- client/src/components/routes/PendingRoute.tsx
+++ client/src/components/routes/PendingRoute.tsx
@@ -7,9 +7,14 @@ import {
     sendAndConfirmTransaction,
     SystemProgram,
     Transaction,
-    TransactionInstruction
+    TransactionInstruction,
 } from '@solana/web3.js';
-import {createAssociatedTokenAccount, getAssociatedTokenAddress, TOKEN_PROGRAM_ID,ASSOCIATED_TOKEN_PROGRAM_ID} from "@solana/spl-token"
+import {
+    createAssociatedTokenAccount,
+    getAssociatedTokenAddress,
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
 import { createTransaction } from '@solana/pay';
 import React, { FC, useEffect, useCallback } from 'react';
 import { useConfig } from '../../hooks/useConfig';
@@ -20,40 +25,42 @@ import { PoweredBy } from '../sections/PoweredBy';
 import { QRCode } from '../sections/QRCode';
 import * as css from './PendingRoute.module.pcss';
 import BN from 'bignumber.js';
-import lo from "buffer-layout"
-import {createAssociatedTokenAccountInstruction} from "@solana/spl-token/src/instructions";
+import lo from 'buffer-layout';
+import { createAssociatedTokenAccountInstruction } from '@solana/spl-token/src/instructions';
 
 export const PendingRoute: FC = () => {
     const { symbol, connectWallet, splToken, recipient } = useConfig();
     const { amount, reset } = usePayment();
-    const { publicKey, sendTransaction,signTransaction } = useWallet();
+    const { publicKey, sendTransaction, signTransaction } = useWallet();
     const { setVisible } = useWalletModal();
 
     const { connection } = useConnection();
 
-    const orcaFarmToken = "orcarKHSqC5CDDsGbho8GKvwExejWHxTqGzXgcewB9L";
-    const faucetUSDT = "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr";
-    const merchantAddress = "5pwgZ5cumBKM56nGAiAXf7UzmYMgjkQswbWbmEBWNvDG";
-    const customerAddress = "78yZfwe88s4GaXhXfKEhMy3vK1wxyrm6Au1T3EsmEaTb";
+    const orcaFarmToken = 'orcarKHSqC5CDDsGbho8GKvwExejWHxTqGzXgcewB9L';
+    const faucetUSDT = 'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr';
+    const merchantAddress = '5pwgZ5cumBKM56nGAiAXf7UzmYMgjkQswbWbmEBWNvDG';
+    const customerAddress = '78yZfwe88s4GaXhXfKEhMy3vK1wxyrm6Au1T3EsmEaTb';
 
     const programAddress = new PublicKey('2nwcNoXkFSgXJHjCW98ACpujPHFJt4LvMKFp83718Vb2');
     const customTokenAddress = new PublicKey(faucetUSDT);
     // const customTokenAccount = new PublicKey('8tS2nZ5w2DiJuQqC1TDYJwPu89EGDbbtoAoeeGCYxXUo');
 
-    async function getOrCreateAssociatedTokenAccount(
-        connection: Connection,
-        mint: PublicKey,
-        publicKey: PublicKey
-    ){
-        const associatedTokenAddress = await getAssociatedTokenAddress(mint, publicKey)
+    async function getOrCreateAssociatedTokenAccount(connection: Connection, mint: PublicKey, publicKey: PublicKey) {
+        const associatedTokenAddress = await getAssociatedTokenAddress(mint, publicKey);
         if (await connection.getAccountInfo(associatedTokenAddress)) {
-            console.log("associated token account", associatedTokenAddress.toString())
-            return associatedTokenAddress
-        }else{
+            console.log('associated token account', associatedTokenAddress.toString());
+            return associatedTokenAddress;
+        } else {
             // await createAssociatedTokenAccount(connection, wallet, mint , wallet.publicKey)
 
-            console.log("create associated token account for", publicKey)
-            const associatedToken = await getAssociatedTokenAddress(mint, publicKey, false, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
+            console.log('create associated token account for', publicKey);
+            const associatedToken = await getAssociatedTokenAddress(
+                mint,
+                publicKey,
+                false,
+                TOKEN_PROGRAM_ID,
+                ASSOCIATED_TOKEN_PROGRAM_ID
+            );
 
             const transaction = new Transaction().add(
                 createAssociatedTokenAccountInstruction(
@@ -71,42 +78,44 @@ export const PendingRoute: FC = () => {
             // await sendAndConfirmTransaction(connection, transaction, [payer], confirmOptions);
         }
 
-
-
         //return await createAssociatedTokenAccount(connection, wallet, mint , wallet.publicKey)
     }
-    const transferOverContract =  async function (){
+    const transferOverContract = async function () {
         console.log('transferOverContract');
-        if(!publicKey || !signTransaction){
+        if (!publicKey || !signTransaction) {
             console.warn('Wallet not connected');
             return;
         }
-        const merchantTokenPubkey = await getOrCreateAssociatedTokenAccount(connection,customTokenAddress,new PublicKey(merchantAddress));
-        const userTokenPubkey = await getOrCreateAssociatedTokenAccount(connection,customTokenAddress,publicKey);
-        const amount = Buffer.alloc(8) // 50 SPL
-        lo.ns64("value").encode(new BN("1000000"), amount)
+        const merchantTokenPubkey = await getOrCreateAssociatedTokenAccount(
+            connection,
+            customTokenAddress,
+            new PublicKey(merchantAddress)
+        );
+        const userTokenPubkey = await getOrCreateAssociatedTokenAccount(connection, customTokenAddress, publicKey);
+        const amount = Buffer.alloc(8); // 50 SPL
+        lo.ns64('value').encode(new BN('1000000'), amount);
         const approveIx = new TransactionInstruction({
             keys: [
-                {pubkey: publicKey, isSigner: true, isWritable: true},
-                {pubkey: userTokenPubkey, isSigner: false, isWritable: true},
-                {pubkey: merchantTokenPubkey, isSigner: false, isWritable: true},
-                {pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false},
+                { pubkey: publicKey, isSigner: true, isWritable: true },
+                { pubkey: userTokenPubkey, isSigner: false, isWritable: true },
+                { pubkey: merchantTokenPubkey, isSigner: false, isWritable: true },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
             ],
             programId: programAddress,
             data: Buffer.of(0, ...amount),
         });
         const transaction = new Transaction().add(approveIx);
-        console.log("approveIx",approveIx);
+        console.log('approveIx', approveIx);
 
         // const signed = await signTransaction(transaction);
         // console.log("signed",signed);
 
         const opts = { signers: publicKey };
 
-        const signature = await sendTransaction(transaction, connection,opts);
-        console.log("signature",signature);
-        const xyx  = await connection.confirmTransaction(signature, 'processed');
-        console.log("signature:", xyx);
+        const signature = await sendTransaction(transaction, connection, opts);
+        console.log('signature', signature);
+        const xyx = await connection.confirmTransaction(signature, 'processed');
+        console.log('signature:', xyx);
 
         //
         // const resApprove = await sendAndConfirmTransaction(connection, new Transaction().add(approveIx), [
@@ -121,39 +130,43 @@ export const PendingRoute: FC = () => {
             [publicKey]
         )*/
         return;
-    }
-    const transferOverContract_bak =  async function (){
+    };
+    const transferOverContract_bak = async function () {
         console.log('transferOverContract');
-        if(!publicKey || !signTransaction){
+        if (!publicKey || !signTransaction) {
             console.warn('Wallet not connected');
             return;
         }
-        const merchantTokenPubkey = await getOrCreateAssociatedTokenAccount(connection,customTokenAddress,new PublicKey(merchantAddress));
-        const userTokenPubkey = await getOrCreateAssociatedTokenAccount(connection,customTokenAddress,publicKey);
-        const amount = Buffer.alloc(8) // 50 SPL
-        lo.ns64("value").encode(new BN("1000000"), amount)
+        const merchantTokenPubkey = await getOrCreateAssociatedTokenAccount(
+            connection,
+            customTokenAddress,
+            new PublicKey(merchantAddress)
+        );
+        const userTokenPubkey = await getOrCreateAssociatedTokenAccount(connection, customTokenAddress, publicKey);
+        const amount = Buffer.alloc(8); // 50 SPL
+        lo.ns64('value').encode(new BN('1000000'), amount);
         const approveIx = new TransactionInstruction({
             keys: [
-                {pubkey: publicKey, isSigner: true, isWritable: true},
-                {pubkey: userTokenPubkey, isSigner: false, isWritable: true},
-                {pubkey: merchantTokenPubkey, isSigner: false, isWritable: true},
-                {pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false},
+                { pubkey: publicKey, isSigner: true, isWritable: true },
+                { pubkey: userTokenPubkey, isSigner: false, isWritable: true },
+                { pubkey: merchantTokenPubkey, isSigner: false, isWritable: true },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
             ],
             programId: programAddress,
             data: Buffer.of(0, ...amount),
         });
         const transaction = new Transaction().add(approveIx);
-        console.log("approveIx",approveIx);
+        console.log('approveIx', approveIx);
 
         // const signed = await signTransaction(transaction);
         // console.log("signed",signed);
 
         const opts = { signers: publicKey };
 
-        const signature = await sendTransaction(transaction, connection,opts);
-        console.log("signature",signature);
-        const xyx  = await connection.confirmTransaction(signature, 'processed');
-        console.log("signature:", xyx);
+        const signature = await sendTransaction(transaction, connection, opts);
+        console.log('signature', signature);
+        const xyx = await connection.confirmTransaction(signature, 'processed');
+        console.log('signature:', xyx);
 
         //
         // const resApprove = await sendAndConfirmTransaction(connection, new Transaction().add(approveIx), [
@@ -168,12 +181,7 @@ export const PendingRoute: FC = () => {
             [publicKey]
         )*/
         return;
-    }
-
-
-
-
-
+    };
 
     const onClick = async function () {
         if (!publicKey || !amount) {
