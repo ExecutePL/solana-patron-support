285c7131be2f81689d0bed0e05e87732e862d112
Bruno <bruno.cisowski@gmail.com>
2022-03-17 13:45:00 +0100

    access uploads

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,9 +9,13 @@ export const App = async () => {
   restApi.use(bodyParser.json());
   restApi.use(cors());
   restApi.use(express.static(path.resolve(__dirname, '../../client/dist')));
+  restApi.use(
+    '/uploads',
+    express.static(path.resolve(__dirname, '../../public/uploads'))
+  );
   restApi.use('/api', router);
   restApi.get('*', (req: express.Request, res: express.Response) => {
-      res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
   });
   return { restApi };
 };
