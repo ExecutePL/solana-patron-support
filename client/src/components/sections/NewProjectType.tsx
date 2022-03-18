94ee4cf12cebe0360d6e27a6d5ff5a36ef3f671a
wojcikfil <94838246+wojcikfil@users.noreply.github.com>
2022-03-18 01:46:39 +0100

    fix: organization description, add squads.so link

--- client/src/components/sections/NewProjectType.tsx
+++ client/src/components/sections/NewProjectType.tsx
@@ -1,26 +1,28 @@
-import React from "react"
+import React from 'react';
 import * as css from './NewProjectType.module.pcss';
-import { Item, RadioGroup } from "../radioGroup/RadioGroup";
+import { Item, RadioGroup } from '../radioGroup/RadioGroup';
 
 interface NewProjectTypeProps {
-    selectedProjectType : string;
-    onTypeClick: (type: string) => void
+    selectedProjectType: string;
+    onTypeClick: (type: string) => void;
 }
 
-export const NewProjectType = ({selectedProjectType, onTypeClick }:NewProjectTypeProps) =>  (
+export const NewProjectType = ({ selectedProjectType, onTypeClick }: NewProjectTypeProps) => (
     <div className={css.container}>
         <p className={css.subtitle}>Choose Your type of fundraising project</p>
-        <RadioGroup items={projectTypes} onItemClick={onTypeClick} selectedItem={selectedProjectType}/> 
+        <RadioGroup items={projectTypes} onItemClick={onTypeClick} selectedItem={selectedProjectType} />
     </div>
-)
+);
 
-const projectTypes:Item[] = [
+const projectTypes: Item[] = [
     {
-        name:'indyvidual',
-        description:'For creators or private collections managed by a single entity. best option for simple projects, creators or startups'
+        name: 'indyvidual',
+        description:
+            'For creators or private collections managed by a single entity. best option for simple projects, creators or startups',
     },
     {
-        name:'organization',
-        description:' For dao organization with donor-managed assets with collaborative infrastructure'
-    }
-]
+        name: 'organization',
+        description: ' For dao organization with donor-managed assets with collaborative infrastructure',
+        link: 'https://squads.so/',
+    },
+];
